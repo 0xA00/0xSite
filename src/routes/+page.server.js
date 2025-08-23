5bcd8aff8323fabcf9a6b589d1f52eafed56f12e
0xa0 <hyp3rcr4ft@gmail.com>
2025-08-23 23:49:11 +0200

    changed back

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,16 +1,18 @@
 import sharp from 'sharp';
-import fs from 'fs';
-import path from 'path';
+import { readdir } from 'fs/promises';
+
 
 
 
 export async function load() {
-    const imgDir = path.join(process.cwd(), 'static/img');
-    const files = fs.readdirSync(imgDir).filter(file => /\.(png|jpg|jpeg)$/i.test(file));
+    const imgDir = `${process.cwd()}/static/img`;
+    const files = (await readdir(imgDir)).filter(file => /\.(png|jpg|jpeg)$/i.test(file));
+    if (files.length === 0) {
+        throw new Error('No image files found in directory');
+    }
     const randomFile = files[Math.floor(Math.random() * files.length)];
-    const imgPath = path.join(imgDir, randomFile);
+    const imgPath = `${imgDir}/${randomFile}`;
     const buffer = await sharp(imgPath)
-        .webp()
         .resize(256, 256, { fit: 'cover' })
         .webp({ quality: 75 })
         .toBuffer();
