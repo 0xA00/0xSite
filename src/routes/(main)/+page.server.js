424d8797ba0b1cdac345d2526238891f2f5dcc95
0xa0 <hyp3rcr4ft@gmail.com>
2026-03-10 23:48:57 +0100

    voila

--- src/routes/(main)/+page.server.js
+++ src/routes/(main)/+page.server.js
@@ -1,17 +1,9 @@
-import sharp from 'sharp';
-import { readdir } from 'fs/promises';
-
-
-
-
 export async function load() {
-
-
-    const modules = import.meta.glob('../*/**/+page.svx', { eager: true });
+  const modules = import.meta.glob('../*/**/+page.svx', { eager: true });
 
   const posts = Object.entries(modules).map(([path, module]) => {
     const parts = path.split('/');
-    const slug = parts[2]; 
+    const slug = parts[2];
 
     return {
       slug,
@@ -21,38 +13,7 @@ export async function load() {
     };
   });
 
-
-    const imgDir = `${process.cwd()}/static/img`;
-    const files = (await readdir(imgDir)).filter(file => /\.(png|jpg|jpeg)$/i.test(file));
-    if (files.length === 0) {
-        throw new Error('No image files found in directory');
-    }
-    const randomFile = files[Math.floor(Math.random() * files.length)];
-    const imgPath = `${imgDir}/${randomFile}`;
-    const buffer = await sharp(imgPath)
-        .webp({ quality: 75 })
-        .toBuffer();
-
-    const base64Image = `data:image/webp;base64,${buffer.toString('base64')}`;
-    if (!base64Image) {
-        return new Response('Failed to convert image', { status: 500 });
-    }
-
-    const gifPath = `${process.cwd()}/static/0xA0.gif`;
-    let base64Gif = null;
-    try {
-      const gifBuffer = await sharp(gifPath)
-        .resize(88, 31, { fit: 'cover' })
-        .gif()
-        .toBuffer();
-      base64Gif = `data:image/gif;base64,${gifBuffer.toString('base64')}`;
-    } catch (e) {
-      base64Gif = null;
-    }
-    
-    return {
-        profileImg: base64Image,
-        posts,
-        profileGif: base64Gif
-    };
+  return {
+    posts
+  };
 }
